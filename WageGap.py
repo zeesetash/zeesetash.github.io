75123694168e1eb9154b3c9dfd1c0492d42d3acc
Zee Setash <azsetash@gmail.com>
2023-10-30 14:05:12 -0400

    creation of new plots

--- WageGap.py
+++ WageGap.py
@@ -1,37 +1,74 @@
 #import packages
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
 #import data from csv
 wage_gap = pd.read_csv("C:/Users/Z/Documents/GitHub/zeesetash/WageGap/EPI Data Library - Gender wage gap.csv")
-print(wage_gap.head(), wage_gap.info())
+wage_am = pd.read_csv("C:/Users/Z/Documents/GitHub/zeesetash/WageGap/EPI Data Library - Medianaverage hourly wages.csv")
+wage_bw = pd.read_csv("C:/Users/Z/Documents/GitHub/zeesetash/WageGap/EPI Data Library - Black-white wage gap.csv")
+wage_hw = pd.read_csv("C:/Users/Z/Documents/GitHub/zeesetash/WageGap/EPI Data Library - Hispanic-white wage gap.csv")
+print(wage_gap.describe(), wage_am.describe(), wage_bw.describe(), wage_hw.describe())
 
 
-#create separate dataframes for wage and wage gap
-gap=[col for col in wage_gap.columns if 'Gap' in col]
-wage_df = wage_gap.drop(gap, axis=1)
-gap_df = wage_gap[gap]
+
+#plot of men, women, and overall
+plt.figure(1)
+plt.plot("Date", "Men Median", data=wage_gap, label= "All Men", color='b')
+plt.plot("Date", "Women Median", data=wage_gap, label= "All Women", color='r')
+plt.plot("Date", "Median", data=wage_am, label= "Median", color='k')
+plt.xlabel("Year")
+plt.ylabel("Median wage in 2022 Dollars")
+plt.ylim(10,30)
+plt.title('Wage by Gender')
+plt.legend()
+plt.show()
+
+#plot of gender gap
+plt.figure(4)
+plt.plot("Date", "Gap (Median)", data=wage_gap )
+plt.xlabel("Year")
+plt.ylabel("Percent Gap of Median")
+plt.title("Wage Gap for Women")
+plt.show()
+
+#plot of men Median wages
+plt.figure(2)
+plt.plot("Date", "Median", data=wage_am, label= "Median", color='k')
+plt.plot("Date", "Men Median", data=wage_gap, label= "All Men", color='b')
+plt.plot("Date", "White Men Median", data=wage_gap, label= "White Men", color='m')
+plt.plot("Date", "Black Men Median", data=wage_gap, label= "Black Men", color='g')
+plt.plot("Date", "Hispanic Men Median", data=wage_gap, label= "Hispanic Men", color='c')
+plt.xlabel("Year")
+plt.ylabel("Median wage in 2022 Dollars")
+plt.title("Men's wages by Race")
+plt.ylim(10,30)
+plt.legend()
+plt.show()
 
 
-#set up combined plot
-fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
-fig.suptitle("Wages Over Time")
+#plot of women Median wages
+plt.figure(3)
+plt.plot("Date", "Median", data=wage_am, label= "Median", color='k')
+plt.plot("Date", "Women Median", data=wage_gap, label= "All Women", color='r')
+plt.plot("Date", "White Women Median", data=wage_gap, label= "White Women", color='m')
+plt.plot("Date", "Black Women Median", data=wage_gap, label= "Black Women", color='g')
+plt.plot("Date", "Hispanic Women Median", data=wage_gap, label= "Hispanic Women", color='c')
+plt.xlabel("Year")
+plt.ylabel("Median wage in 2022 Dollars")
+plt.title("Women's wages by Race")
+plt.ylim(10,30)
+plt.legend()
+plt.show()
 
-#plot of men average wages
-ax1.plot("Date", "Men Average", data=wage_df, label= "All Men")
-ax1.plot("Date", "White Men Average", data=wage_df, label= "White Men")
-ax1.plot("Date", "Black Men Average", data=wage_df, label= "Black Men")
-ax1.plot("Date", "Hispanic Men Average", data=wage_df, label= "Hispanic Men")
-ax1.legend()
-
-
-#plot of women average wages
-ax2.plot("Date", "Women Average", data=wage_df, label= "All Women")
-ax2.plot("Date", "White Women Average", data=wage_df, label= "White Women")
-ax2.plot("Date", "Black Women Average", data=wage_df, label= "Black Women")
-ax2.plot("Date", "Hispanic Women Average", data=wage_df, label= "Hispanic Women")
-ax2.legend()
-
-fig.tight_layout()
+#plot of Median wages by race
+plt.figure(5)
+plt.plot("Date",  "Median", data=wage_am, label= "Median", color='k')
+plt.plot("Date", "White Median", data=wage_bw, label= "White Median", color='m')
+plt.plot("Date", "Black Median", data=wage_bw, label= "Black Median", color='g')
+plt.plot("Date", "Hispanic Median", data=wage_hw, label= "Hispanic Median", color='c')
+plt.xlabel("Year")
+plt.ylabel("Median wage in 2022 Dollars")
+plt.title("Wages by Race")
+plt.ylim(10,30)
+plt.legend()
 plt.show()
